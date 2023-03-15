7cce0671a5dbedd5a07b4634bce0af0cafca84ed
Sheddyranking <shedyisaac@gmail.com>
2023-03-15 17:15:28 +0100

    Table scripts for window functions

---
+++ employee.sql
@@ -0,0 +1,26 @@
+create table  employee (
+	emp_id Bigserial NOT NULL PRIMARY KEY,
+	emp_name VARCHAR(50) NOT NULL,
+	emp_dept VARCHAR(50) NOT NULL,
+	salary INT NOT NULL
+);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (1, 'Tomini', 'Marketing', 3244);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (2, 'Witcher', 'Accounting', 4349);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (3, 'Nickell', 'Engineering', 2693);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (4, 'Greenhowe', 'Services', 3780);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (5, 'Nissle', 'Product Management', 1303);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (6, 'Merrall', 'Accounting', 3771);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (7, 'Gumb', 'Training', 1108);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (8, 'Ivanaev', 'Marketing', 1376);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (9, 'Anger', 'Human Resources', 5291);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (10, 'Borth', 'Sales', 3868);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (11, 'Drewe', 'Engineering', 4783);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (12, 'Rivel', 'Human Resources', 1166);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (13, 'Cartwright', 'Human Resources', 2699);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (14, 'Salvati', 'Accounting', 3342);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (15, 'Longcaster', 'Human Resources', 2333);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (16, 'Pyatt', 'Legal', 3386);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (17, 'Secret', 'Business Development', 4256);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (18, 'Janko', 'Business Development', 1782);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (19, 'Ehrat', 'Engineering', 1897);
+insert into  employee (emp_id, emp_name, emp_dept, salary) values (20, 'Filochov', 'Training', 4917);
